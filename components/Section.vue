42ecf0813fc1e54dee4e6071b756859350651f50
Shubham Mehta <Shubham Mehta>
2020-02-28 11:58:36 +0530

    Section Component

---
+++ components/Section.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <Tiles />
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+import Tiles from './Tiles'
+export default {
+  components: {
+    Tiles
+  }
+}
+</script>
