6b07455d2c6833894c87a85dea21f26df4cb4f71
Shubham Mehta <Shubham Mehta>
2020-02-28 14:51:38 +0530

    Changed Dropdown Location

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -24,22 +24,21 @@
           <a class="navbar-item">Home</a>
 
           <a class="navbar-item">Documentation</a>
-
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">More</a>
-
-            <div class="navbar-dropdown">
-              <a class="navbar-item">About</a>
-              <a class="navbar-item">Jobs</a>
-              <a class="navbar-item">Contact</a>
-              <hr class="navbar-divider" />
-              <a class="navbar-item">Report an issue</a>
-            </div>
-          </div>
         </div>
 
         <div class="navbar-end">
           <div class="navbar-item">
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">More</a>
+
+              <div class="navbar-dropdown">
+                <a class="navbar-item">About</a>
+                <a class="navbar-item">Jobs</a>
+                <a class="navbar-item">Contact</a>
+                <hr class="navbar-divider" />
+                <a class="navbar-item">Report an issue</a>
+              </div>
+            </div>
             <div class="buttons">
               <a class="button is-primary">
                 <strong>Sign up</strong>
